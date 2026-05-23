9e004e1776a66c411dfc484523fadbacb936fbf9
krishna <venkatakrishnaredd450@gmail.com>
2026-05-23 17:26:19 +0530

    actions

--- 00-vpc/main.tf
+++ 00-vpc/main.tf
@@ -1,5 +1,5 @@
 module "vpc"{
-    source = "../../aws-vpc-module-terraform"
+    source = "git::https://github.com/krishna-33s/aws-vpc-module-terraform.git?ref=main"
     project = var.project
     env = var.env
     vpc_peering = true
