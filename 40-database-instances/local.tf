53fa751ad128985e7285df2cd5ed7f6eea890ed7
krishna <venkatakrishnaredd450@gmail.com>
2026-05-26 17:14:07 +0530

    changes

--- 40-database-instances/local.tf
+++ 40-database-instances/local.tf
@@ -1,7 +1,7 @@
 locals {
     ami_id = data.aws_ami.redhat.id
-    public_subnet_id = split(",",data.aws_ssm_parameter.database_subnet_id.value)[0]
-    bastion_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
+    database_subnet_id = split(",",data.aws_ssm_parameter.database_subnet_id.value)[0]
+    mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
     common_tags = {
         Project = var.project
         Environment = var.env
