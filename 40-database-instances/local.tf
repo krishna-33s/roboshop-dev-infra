d0bde12a96d0a8d1557b5b74f0b04d0c37efe405
krishna <venkatakrishnaredd450@gmail.com>
2026-05-27 09:33:45 +0530

    actions

--- 40-database-instances/local.tf
+++ 40-database-instances/local.tf
@@ -1,7 +1,7 @@
 locals {
     ami_id = data.aws_ami.redhat.id
     database_subnet_id = split(",",data.aws_ssm_parameter.database_subnet_id.value)[0]
-    mongodb_sg_id = [data.aws_ssm_parameter.mongodb_sg_id.value]
+    mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
     common_tags = {
         Project = var.project
         Environment = var.env
