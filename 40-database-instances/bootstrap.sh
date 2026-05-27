645490ce79ea0f07b1472a39cf99a8aa73ad956e
krishna <venkatakrishnaredd450@gmail.com>
2026-05-27 17:24:28 +0530

    actions

--- 40-database-instances/bootstrap.sh
+++ 40-database-instances/bootstrap.sh
@@ -3,6 +3,7 @@
 module=$1
 dnf install ansible -y
 cd /home/ec2-user
+rm -rf ansible-roles-terraform
 git clone https://github.com/krishna-33s/ansible-roles-terraform.git
 
 cd ansible-roles-terraform
