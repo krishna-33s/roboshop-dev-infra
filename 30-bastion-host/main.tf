74124b41171cb48edbdf225fb9085772a99109d1
krishna <venkatakrishnaredd450@gmail.com>
2026-05-27 14:17:34 +0530

    actions

--- 30-bastion-host/main.tf
+++ 30-bastion-host/main.tf
@@ -4,6 +4,19 @@ resource "aws_instance" "example" {
   subnet_id = local.public_subnet_id
   vpc_security_group_ids = [local.bastion_sg_id]
   iam_instance_profile = aws_iam_instance_profile.bastiondev.name
+  user_data=file("bastion.sh")
+
+  root_block_device {
+   
+    volume_size = 50
+    volume_type = "gp3"
+    
+    tags = merge(
+    { Name = "bastion-${var.project}-${var.env}" },
+    local.common_tags
+  )
+
+  }
 
   tags = merge(
     { Name = "bastion-${var.project}-${var.env}" },
