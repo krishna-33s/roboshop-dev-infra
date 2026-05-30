caed329d9ac97f76b330dae0b82c3c8faf9740bb
krishna <venkatakrishnaredd450@gmail.com>
2026-05-30 19:14:24 +0530

    changes

--- 20-sg-roles/main.tf
+++ 20-sg-roles/main.tf
@@ -1,3 +1,4 @@
+# bastion accepting interenet 
 resource "aws_security_group_rule" "bastion_internet" {
   type              = "ingress"
   from_port         = 22
@@ -8,6 +9,7 @@ resource "aws_security_group_rule" "bastion_internet" {
   security_group_id = local.bastion_sg_id
 }
 
+# mongodb accepting bastion
 resource "aws_security_group_rule" "mongodb_bastion" {
   type              = "ingress"
   from_port         = 22
@@ -17,6 +19,7 @@ resource "aws_security_group_rule" "mongodb_bastion" {
   security_group_id = local.mongodb_sg_id
 }
 
+# redis accepting bastion
 resource "aws_security_group_rule" "redis_bastion" {
   type              = "ingress"
   from_port         = 22
@@ -26,6 +29,7 @@ resource "aws_security_group_rule" "redis_bastion" {
   security_group_id = local.redis_sg_id
 }
 
+# mysql accepting bastion
 resource "aws_security_group_rule" "mysql_bastion" {
   type              = "ingress"
   from_port         = 22
@@ -35,6 +39,7 @@ resource "aws_security_group_rule" "mysql_bastion" {
   security_group_id = local.mysql_sg_id
 }
 
+# rabbitmq accepting bastion
 resource "aws_security_group_rule" "rabbitmq_bastion" {
   type              = "ingress"
   from_port         = 22
@@ -44,6 +49,7 @@ resource "aws_security_group_rule" "rabbitmq_bastion" {
   security_group_id = local.rabbitmq_sg_id
 }
 
+# mongodb --> catalogue
 resource "aws_security_group_rule" "mongodb_catalogue" {
   type              = "ingress"
   from_port         = 27017
@@ -53,7 +59,7 @@ resource "aws_security_group_rule" "mongodb_catalogue" {
   security_group_id = local.mongodb_sg_id
 }
 
-
+# mongodb --> user
 resource "aws_security_group_rule" "mongodb_user" {
   type              = "ingress"
   from_port         = 27017
@@ -63,6 +69,7 @@ resource "aws_security_group_rule" "mongodb_user" {
   security_group_id = local.mongodb_sg_id
 }
 
+# backend_ALB accepting bastion
 resource "aws_security_group_rule" "backend_ALB_bastion" {
   type              = "ingress"
   from_port         = 80
@@ -70,6 +77,26 @@ resource "aws_security_group_rule" "backend_ALB_bastion" {
   protocol          = "tcp"
   source_security_group_id = local.bastion_sg_id
   security_group_id = local.backend_ALB_sg_id
+}
+
+# catalogue accepting bastion
+resource "aws_security_group_rule" "catalogue_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  source_security_group_id = local.bastion_sg_id
+  security_group_id = local.catalogue_sg_id
+}
+
+# catalogue accepting backend_ALB
+resource "aws_security_group_rule" "catalogue_backend_ALB" {
+  type              = "ingress"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  source_security_group_id = local.backend_ALB_sg_id
+  security_group_id = local.catalogue_sg_id
 }
 
 
