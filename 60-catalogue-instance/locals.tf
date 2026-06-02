4e9bcb895e973d28c56105fedd949983ab57a724
krishna <venkatakrishnaredd450@gmail.com>
2026-06-02 07:59:05 +0530

    actions

--- 60-catalogue-instance/locals.tf
+++ 60-catalogue-instance/locals.tf
@@ -1,4 +1,5 @@
 locals {
+    ami_id = data.aws_ami.redhat.id
     private_subnet_id = split(",",data.aws_ssm_parameter.private_subnet_id.value)[0]
     catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
     common_tags = {
