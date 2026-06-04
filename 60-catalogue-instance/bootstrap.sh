e75de3069d9efac300f66dbdf32c8bf8e652c650
krishna <venkatakrishnaredd450@gmail.com>
2026-06-04 20:26:05 +0530

    terraform

--- 60-catalogue-instance/bootstrap.sh
+++ 60-catalogue-instance/bootstrap.sh
@@ -1,11 +1,24 @@
+# #!/bin/bash
+
+# module=$1
+# env=$2
+# dnf install ansible -y
+# cd /home/ec2-user
+# rm -rf ansible-roles-terraform
+# git clone https://github.com/krishna-33s/ansible-roles-terraform.git
+
+# cd ansible-roles-terraform
+# ansible-playbook -e module=$module -e env=$env roboshop.yml
+
 #!/bin/bash
 
 module=$1
 env=$2
 dnf install ansible -y
+
 cd /home/ec2-user
-rm -rf ansible-roles-terraform
-git clone https://github.com/krishna-33s/ansible-roles-terraform.git
+git clone https://github.com/daws-88s/ansible-roles-terraform.git
 
 cd ansible-roles-terraform
-ansible-playbook -e module=$module -e env=$env roboshop.yml
+git pull
+ansible-playbook -e module=$module -e env=$env  roboshop.yaml
