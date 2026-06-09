90ab789289e18fdd3fbd26e1935cd5e3d24c0bf7
krishna <venkatakrishnaredd450@gmail.com>
2026-06-09 22:38:56 +0530

    terraform

---
+++ 70-acm/variable.tf
@@ -0,0 +1,15 @@
+variable "project"{
+    default = "roboshop"
+}
+
+variable "env"{
+    default = "dev"
+}
+
+variable "domain_name"{
+    default = "krishnadev.space"
+}
+
+variable "zone_id"{
+    default = "Z02917532NKN3LI0A0XT4"
+}
