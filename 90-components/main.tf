e24eddd106da9cd4c246af02513e4c476d3efa9c
krishna <venkatakrishnaredd450@gmail.com>
2026-06-11 21:13:46 +0530

    terraform

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -2,5 +2,5 @@ module "module" {
     for_each = var.module
     source = "git::https://github.com/krishna-33s/terraform-components-roboshop.git?ref=main" 
     module = each.key
-    priority = each.priority.value
+    priority = each.value.priority
 }
