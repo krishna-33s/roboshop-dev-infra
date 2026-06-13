4cc225860234a63a08c52865d8eb692c366c388a
krishna <venkatakrishnaredd450@gmail.com>
2026-06-13 16:20:18 +0530

    terraform

--- 90-components/variable.tf
+++ 90-components/variable.tf
@@ -4,18 +4,18 @@ variable "module" {
         catalogue ={
             priority = 10
         }
-        user ={
-            priority = 20
-        }
-        cart ={
-            priority = 30
-        }
-        shipping ={
-            priority = 40
-        }
-        payment ={
-            priority = 50
-        }
+        # user ={
+        #     priority = 20
+        # }
+        # cart ={
+        #     priority = 30
+        # }
+        # shipping ={
+        #     priority = 40
+        # }
+        # payment ={
+        #     priority = 50
+        # }
         # frontend components attach to frontendalb
         frontend ={
             priority = 10
