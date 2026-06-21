0e43af1d74233aa4ae866a5f470086a06b84a654
krishna <venkatakrishnaredd450@gmail.com>
2026-06-21 15:06:41 +0530

    terraform

--- 30-bastion-host/bastion.sh
+++ 30-bastion-host/bastion.sh
@@ -9,3 +9,20 @@ xfs_growfs /home
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 sudo yum -y install terraform
+
+
+# creating databases
+cd /home/ec2-user
+git clone https://github.com/daws-88s/roboshop-infra-dev.git
+chown ec2-user:ec2-user -R roboshop-infra-dev
+cd roboshop-infra-dev/40-databases
+terraform init
+terraform apply -auto-approve
+
+# creating components
+cd /home/ec2-user
+git clone https://github.com/daws-88s/roboshop-infra-dev.git
+chown ec2-user:ec2-user -R roboshop-infra-dev
+cd roboshop-infra-dev/90-components
+terraform init
+terraform apply -auto-approve
